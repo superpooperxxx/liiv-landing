da86d89f9b0723fc939122a0c058cf564f4e462d
Kyrylo Haiduk <kyrylo@talentpath.co>
2024-09-30 17:30:49 +0300

    added lenis

--- src/script.js
+++ src/script.js
@@ -2,6 +2,22 @@ import { SplitText } from "./scripts/SplitText";
 
 gsap.registerPlugin(ScrollTrigger);
 
+// LENIS
+const lenis = new Lenis({
+  lerp: 0.05,
+});
+
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
 // AFFORDABLE
 const affordableLinePath = document.querySelector(".affordable-line > path");
 const affordableLinePathLength = affordableLinePath.getTotalLength();
